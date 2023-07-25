f9e4c3e72fd77bd7e50277b018e6134897ca34bf
Aaron <102457247+Chrollo970@users.noreply.github.com>
2023-07-25 06:18:32 -0700

    added some grids and frame

--- calc.py
+++ calc.py
@@ -30,65 +30,72 @@ def clear():
     
 
 window = tk.Tk()
-window.title("Calculator")
+window.title("Python Calculator")
+window.geometry('270x212')
+window.resizable(False,False)
+
+windowIcon = tk.PhotoImage(file='logo.png')
+window.iconphoto(True,windowIcon)
 
 equationText = ""
 equationLabel = tk.StringVar()
 
-container = tk.Frame(window)
-container.pack()
+BUTTONSIZE = 8
 
-lbl = tk.Label(window,textvariable=equationLabel, bg='white', width=60, height=4)
+lbl = tk.Label(window,textvariable=equationLabel, bg='white', width=30, height=4,font=('consolas',12))
 lbl.pack()
+frame = tk.Frame(window)
+frame.pack()
 
-button1 = tk.Button(window,text='1',width=5, command=lambda : press(1))
-button1.pack()
+button1 = tk.Button(frame,text='1',width=BUTTONSIZE, command=lambda : press(1))
+button1.grid(row=1,column=0)
 
-button2 = tk.Button(window,text='2',width=5,command=lambda : press(2))
-button2.pack()
+button2 = tk.Button(frame,text='2',width=BUTTONSIZE,command=lambda : press(2))
+button2.grid(row=1,column=1)
 
-button3 = tk.Button(window,text='3',width=5,command=lambda : press(3))
-button3.pack()
+button3 = tk.Button(frame,text='3',width=BUTTONSIZE,command=lambda : press(3))
+button3.grid(row=1,column=2)
 
-btnPlus = tk.Button(window, text='+', width=5,command=lambda : press('+'))
-btnPlus.pack()
+btnPlus = tk.Button(frame, text='+', width=BUTTONSIZE,command=lambda : press('+'))
+btnPlus.grid(row=1,column=3)
 
-button4 = tk.Button(window,text='4',width=5,command=lambda : press(4))
-button4.pack()
+button4 = tk.Button(frame,text='4',width=BUTTONSIZE,command=lambda : press(4))
+button4.grid(row=2,column=0)
 
-button5 = tk.Button(window,text='5',width=5,command=lambda : press(5))
-button5.pack()
+button5 = tk.Button(frame,text='5',width=BUTTONSIZE,command=lambda : press(5))
+button5.grid(row=2,column=1)
 
-button6 = tk.Button(window,text='6',width=5,command=lambda : press(6))
-button6.pack()
+button6 = tk.Button(frame,text='6',width=BUTTONSIZE,command=lambda : press(6))
+button6.grid(row=2,column=2)
 
-btnMinus = tk.Button(window,text='-',width=5,command=lambda : press('-'))
-btnMinus.pack()
+btnMinus = tk.Button(frame,text='-',width=BUTTONSIZE,command=lambda : press('-'))
+btnMinus.grid(row=2,column=3)
 
-button7 = tk.Button(window,text='7',width=5,command=lambda : press(7))
-button7.pack()
+button7 = tk.Button(frame,text='7',width=BUTTONSIZE,command=lambda : press(7))
+button7.grid(row=3,column=0)
 
-button8 = tk.Button(window,text='8',width=5,command=lambda : press(8))
-button8.pack()
+button8 = tk.Button(frame,text='8',width=BUTTONSIZE,command=lambda : press(8))
+button8.grid(row=3,column=1)
 
-button9 = tk.Button(window,text='9',width=5,command=lambda : press(9))
-button9.pack()
+button9 = tk.Button(frame,text='9',width=BUTTONSIZE,command=lambda : press(9))
+button9.grid(row=3,column=2)
 
-btnMultiply = tk.Button(window,text='x',width=5,command=lambda : press('*'))
-btnMultiply.pack()
+btnMultiply = tk.Button(frame,text='x',width=BUTTONSIZE,command=lambda : press('*'))
+btnMultiply.grid(row=3,column=3)
 
-btnDivide = tk.Button(window,text='/',width=5,command=lambda : press('/'))
-btnDivide.pack()
+btnDivide = tk.Button(frame,text='/',width=BUTTONSIZE,command=lambda : press('/'))
+btnDivide.grid(row=4,column=0)
 
-btnPeriod = tk.Button(window,text='.', width=5,command=lambda : press('.'))
+btnPeriod = tk.Button(frame,text='.', width=BUTTONSIZE,command=lambda : press('.'))
+btnPeriod.grid(row=4,column=1)
 
-button0 = tk.Button(window,text='0',width=5,command=lambda : press(0))
-button0.pack()
+button0 = tk.Button(frame,text='0',width=BUTTONSIZE,command=lambda : press(0))
+button0.grid(row=4,column=2)
 
-btnEqual = tk.Button(window,text='=',width=5,command=lambda : equal())
-btnEqual.pack()
+btnEqual = tk.Button(frame,text='=',width=BUTTONSIZE,command=lambda : equal())
+btnEqual.grid(row=4,column=3)
 
-btnClear = tk.Button(window,text='Clear',width=5, command=lambda: clear())
+btnClear = tk.Button(window,text='Clear',width=32, command=lambda: clear())
 btnClear.pack()
 
 window.mainloop()
